79d3841a99e8b7e1b6588d2c19f5ba4d417ade9a
laurachebet1997 <rosechebet19971997@gmail.com>
2022-11-24 12:01:11 +0300

    correction

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -11,26 +11,23 @@ size_t binary_tree_size2(const binary_tree_t *tree);
 
 size_t binary_tree_size2(const binary_tree_t *tree)
 {
-  int cont1 = 0, cont2 = 0;
+	int cont1 = 0, cont2 = 0;
 
-  if (tree == NULL)
-    return (0);
+	if (tree == NULL)
+		return (0);
+	if (tree->left != NULL)
+	{
+		cont1 = binary_tree_size2(tree->left);
+		cont1++;
+	}
 
-  if (tree->left != NULL)
-    {
+	if (tree->right != NULL)
+	{
+		cont2 = binary_tree_size2(tree->right);
+		cont2++;
+	}
 
-      cont1 = binary_tree_size2(tree->left);
-      cont1++;
-    }
-
-  if (tree->right != NULL)
-    {
-
-      cont2 = binary_tree_size2(tree->right);
-      cont2++;
-    }
-
-  return (cont1 + cont2);
+	return (cont1 + cont2);
 }
 
 /**
@@ -43,8 +40,7 @@ size_t binary_tree_size2(const binary_tree_t *tree)
 
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-  if (tree == NULL)
-    return (0);
-
-  return (binary_tree_size2(tree) + 1);
+	if (tree == NULL)
+		return (0);
+	return (binary_tree_size2(tree) + 1);
 }
